ae680266c22435ac0630f8c8e19c5852e3b16ad1
alexweb3dev0x <alexweb3dev0x@proton.me>
2024-02-21 03:30:59 +0100

    Refactor timer component and update button labels

--- app/components/timer.ts
+++ app/components/timer.ts
@@ -13,7 +13,7 @@ import { getWinners, getWinningStatus } from "../lib/db";
 import { FrameActionMessage } from "@farcaster/core";
 export async function getTimer(message: FrameActionDataParsedAndHubContext) {
   const total =
-    Date.parse("2024-02-21 02:54:00 GMT+0100") -
+    Date.parse("2024-02-21 03:16:00 GMT+0100") -
     Date.parse(new Date().toString());
   function getTimeRemaining(total: any) {
     const seconds = Math.floor((total / 1000) % 60);
@@ -59,11 +59,16 @@ export async function getTimer(message: FrameActionDataParsedAndHubContext) {
     // const hash = "0x9d021952c7208c3a120aeea23441836a6870acba"
     // const fid = 292319
 
-    const hash = message?.castId?.hash
+    const hash = message?.castId?.hash;
     const fid = message?.requesterFid;
     const creatorFid = message.castId?.fid;
     //@ts-ignore
-    console.log("msg, hash, fid, message.data", message?.castId?.hash, hash, fid);
+    console.log(
+      "msg, hash, fid, message.data",
+      message?.castId?.hash,
+      hash,
+      fid
+    );
     await getWinners(fid, hash);
     const win = await getWinningStatus(fid);
 
@@ -73,13 +78,13 @@ export async function getTimer(message: FrameActionDataParsedAndHubContext) {
         image: `${process.env.NEXT_PUBLIC_HOST}/congratz.jpg`,
         buttons: [
           {
-            label: `Back`,
-            action: "post",
-          },
-          {
             label: `Website`,
             action: "link",
             target: "https://mrphs.io",
+          },
+          {
+            label: `Back`,
+            action: "post",
           },
         ],
         postUrl: `${process.env.NEXT_PUBLIC_HOST}/main`,
@@ -98,13 +103,13 @@ export async function getTimer(message: FrameActionDataParsedAndHubContext) {
         image: `${process.env.NEXT_PUBLIC_HOST}/lose.jpg`,
         buttons: [
           {
-            label: `Back`,
-            action: "post",
-          },
-          {
             label: `Website`,
             action: "link",
             target: "https://mrphs.io",
+          },
+          {
+            label: `Back`,
+            action: "post",
           },
         ],
         postUrl: `${process.env.NEXT_PUBLIC_HOST}/main`,
